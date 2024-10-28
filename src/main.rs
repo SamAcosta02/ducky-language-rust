c3bda6d3f2e45a51684aa4116f6fb99eb60de92d
SamAcosta02 <A00833547@tec.mx>
2024-10-27 22:59:42 -0600

    feat: added new testing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{collections::HashSet, fs};
 
 use pest::Parser;
 use pest_derive::Parser;
@@ -7,24 +7,65 @@ use pest_derive::Parser;
 #[grammar = "patito.pest"]
 pub struct PatitoParser;
 
-fn print_rules(pair: pest::iterators::Pair<Rule>, indent: usize) {
-    // Print the current rule with indentation
-    println!("{:indent$}{:?}", "", pair.as_rule(), indent = indent);
+fn visualize_pair(
+    pair: pest::iterators::Pair<Rule>,
+    context_stack: &mut Vec<Rule>,
+    main_rules: &HashSet<Rule>
+) {
+    let current_rule = pair.as_rule();
+    println!("{:#?}", current_rule);
+    println!("{:#?}", pair.as_str());
 
-    // Iterate through all inner pairs and print their rules
+    // Push the current rule to the context stack, if it is a main rule
+    if main_rules.contains(&current_rule) {
+        println!("Rule: ++{:?}++ IS a main rule", current_rule);
+        context_stack.push(current_rule);
+    } else {
+        println!("Rule: **{:?}** not a main rule", current_rule);
+    }
+
+    match pair.as_rule() {
+        Rule::programKeyword => {
+            println!("ACTION: Create dir_func \n");
+        }
+        Rule::id => {
+            if let Some(&parent_rule) = context_stack.iter().rev().next() {
+                match parent_rule {
+                    Rule::program => println!("ACTION: Add id-name and type program to dir_func \n"),
+                    Rule::vars => println!("ACTION: Search for id-name in current VarTable \n"),
+                    _ => println!("id in another context."),
+                }
+            }
+        }
+        Rule::varsKeyword => {
+            println!("ACTION: if current Func doesn't have a VarTable then create Var Table and link it to current func \n");
+        }
+        _ => {
+            println!("... \n");
+        }
+    }
+    println!("CONTEXT STACK {:?}", &context_stack);
     for inner_pair in pair.into_inner() {
-        print_rules(inner_pair, indent + 2); // Increase indentation for inner rules
+        visualize_pair(inner_pair, context_stack, main_rules);
     }
 }
 
 fn main() {
-    let path = "C:/Users/wetpe/Documents/Tec8/Testing Rust/patito-parser/src/patitos/app.pat";
+    let path = "C:/Users/wetpe/OneDrive/Documents/_Manual/TEC 8/ducky-language-rust/src/tests/app1.dky";
     let patito_file = fs::read_to_string(&path).expect("error reading file");
+
+    // let mut dir_func: HashMap<String, String> = HashMap::new();
+    let mut context_stack: Vec<Rule> = Vec::new();
+    let main_rules: HashSet<Rule> = [Rule::program, Rule::vars]
+        .iter()
+        .cloned()
+        .collect();
+
     match PatitoParser::parse(Rule::program, &patito_file) {
         Ok(pairs) => {
             for pair in pairs {
-                // println!("{:#?}", pair.as_rule());
-                print_rules(pair, 0);
+                // println!("{:#?}", pair.into_inner());
+                visualize_pair(pair, &mut context_stack, &main_rules);
             }
         }
         Err(e) => {
