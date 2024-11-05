77cec0a8360001af23211de4948bf9cf6c0059a7
SamAcosta02 <A00833547@tec.mx>
2024-11-05 13:28:56 -0600

    first advancement for expression semantics

--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,6 @@ use pest_derive::Parser;
 #[grammar = "patito.pest"]
 pub struct PatitoParser;
 
-fn generate_quads(
-    semantic_cube: &[[[&str; 8]; 2]; 2],
-    current_expr: &mut VecDeque<[&str; 4]>,
-    poper: &mut Vec<char>,
-    op: &mut Vec<[String; 2]>,
-    func_dir: &HashMap<String, HashMap<String, String>>,
-    current_func: &String,
-    pair: &pest::iterators::Pair<Rule>
-) {
-    for inner_pair in pair.into_inner() {
-        generate_quads(semantic_cube, current_expr, poper, op, func_dir, current_func, pair);
-    }
-}
-
 fn visualize_pair(
     pair: pest::iterators::Pair<Rule>,
     context_stack: &mut Vec<Rule>,
@@ -30,26 +16,27 @@ fn visualize_pair(
     semantic_cube: &[[[&str; 8]; 2]; 2],
     context_ids: &mut Vec<String>,
     current_func: &mut String,
-    current_expr: &mut VecDeque<[&str; 4]>
+    quadruples: &mut VecDeque<[&str; 4]>,
+    pending_statements: &mut Vec<String>
 ) {
 
     // visualize current rule
     let current_rule = pair.as_rule();
-    println!("{:#?}", current_rule);
-    println!("{:#?}", pair.as_str());
+    println!("Rule: {:#?}", current_rule);
+    // println!("{:#?}", pair.as_str());
 
     // Push the current rule to the context stack, if it is a main rule
     if main_rules.contains(&current_rule) {
-        println!("Rule: ++{:?}++ IS a main rule", current_rule);
+        // println!("Rule: ++{:?}++ IS a main rule", current_rule);
         context_stack.push(current_rule);
     } else {
-        println!("Rule: **{:?}** not a main rule", current_rule);
+        // println!("Rule: **{:?}** not a main rule", current_rule);
     }
 
     // match the current rule depending on the context
     match pair.as_rule() {
         Rule::programKeyword => {
-            println!("ACTION: Create dir_func \n"); //1
+            // println!("ACTION: Create dir_func \n"); //1
             func_dir.insert(String::from("global"), HashMap::new());
             *current_func = String::from("global");
             println!("{:#?}", func_dir);
@@ -59,28 +46,28 @@ fn visualize_pair(
                 // Check in what context was id matched in
                 match parent_rule {
                     Rule::program => {
-                        println!("ACTION: Add id-name and type program to dir_func \n"); //2
+                        // println!("ACTION: Add id-name and type program to dir_func \n"); //2
                     },
                     Rule::vars => {
-                        println!("ACTION: Search for id-name in current VarTable"); //5
+                        // println!("ACTION: Search for id-name in current VarTable"); //5
                         // Look for the id in the current function
                         // If it is panic as it is already declared
                         if func_dir.get_mut(current_func).unwrap().contains_key(pair.as_str()) {
                             panic!("ERROR: id {} already exists in current context", pair.as_str());
                         // otherwise insert the id to the context-ids stack (to later add them to the dir_func)
                         } else {
-                            println!("ACTION: Add id-name to context-ids to later add them to dir_func \n");
+                            // println!("ACTION: Add id-name to context-ids to later add them to dir_func \n");
                             context_ids.push(pair.as_str().to_string());
                         }
                     },
                     Rule::funcs => {
                         // Update the current function id and add it to the func_dir if it doesn't already exist
-                        println!("ACTION: Add id-name and type func to func_dir \n"); //2
+                        // println!("ACTION: Add id-name and type func to func_dir \n"); //2
                         *current_func = pair.as_str().to_string();
 
                         if !func_dir.contains_key(&current_func.to_string()) {
                             func_dir.insert(current_func.to_string(), HashMap::new());
-                            println!("Function '{}' added to func_dir.", current_func);
+                            // println!("Function '{}' added to func_dir.", current_func);
                         } else {
                             panic!("ERROR: Function '{}' already exists.", current_func);
                         }
@@ -90,22 +77,23 @@ fn visualize_pair(
                         // Add the parameter ID to the contex_ids stack
                         context_ids.push(pair.as_str().to_string());
                     },
-                    Rule::statement => {
+                    _ => {
+                        // if the id is not found in any of these contexts, check if the id exists in the current context
+                        println!("Rule is: {:?}", parent_rule);
                         if func_dir.get_mut(current_func).unwrap().contains_key(pair.as_str()) {
-                            println!("ACTION: Search for id-name in current VarTable");
+                            // println!("ACTION: Search for id-name in current VarTable");
                         } else {
                             panic!("Error: id '{}' not found in current context", pair.as_str());
                         }
-                    }
-                    _ => println!("id in another context."),
+                    },
                 }
             }
         }
         Rule::varsKeyword => {
-            println!("ACTION: if current Func doesn't have a VarTable then create Var Table and link it to current func \n"); //3
+            // println!("ACTION: if current Func doesn't have a VarTable then create Var Table and link it to current func \n"); //3
         }
         Rule::typeVar => {
-            println!("ACTION: update current-type to {} \n", pair.as_str().to_string()); //4
+            // println!("ACTION: update current-type to {} \n", pair.as_str().to_string()); //4
             // Get the currenty type
             *current_type = pair.as_str().to_string();
 
@@ -122,14 +110,32 @@ fn visualize_pair(
             println!("{:#?}", func_dir);
         },
         Rule::beginKeyword => {
-            println!("ACTION: Changing back to the global context \n");
+            // println!("ACTION: Changing back to the global context \n");
             *current_func = "global".to_string();
         },
-        Rule::statement => {
-            let mut poper: Vec<char> = Vec::new();
-            let mut op: Vec<[String; 2]> = Vec::new();
-            let quads = generate_quads(semantic_cube, current_expr, &mut poper, &mut op, &func_dir, current_func, &pair);
-            println!("{:#?}", quads);
+        Rule::assign => {
+            println!("QUAD INFO: assignment going on here");
+            pending_statements.push("assign".to_string());
+        }
+        Rule::condition => {
+            println!("QUAD INFO: condition going on here");
+            pending_statements.push("condition".to_string());
+        }
+        Rule::while_loop => {
+            println!("QUAD INFO: while_loop going on here");
+            pending_statements.push("while_loop".to_string());
+        }
+        Rule::func_call => {
+            println!("QUAD INFO: func_call going on here");
+            pending_statements.push("func_call".to_string());
+        }
+        Rule::print => {
+            println!("QUAD INFO: print going on here");
+            pending_statements.push("print".to_string());
+        }
+        Rule::delimiter => {
+            pending_statements.pop();
+            println!("QUAD INFO: current statement is now: {:?} \n", pending_statements.last());
         }
         _ => {
             println!("... \n");
@@ -137,9 +143,10 @@ fn visualize_pair(
     }
     
     // Check current stack status
-    println!("CONTEXT IDs: {:?}", context_ids);
-    println!("CONTEXT STACK {:?}", &context_stack);
-    println!("CURRENT FUNC {:?}", current_func);
+    // println!("CONTEXT IDs: {:?}", context_ids);
+    // println!("CONTEXT STACK {:?}", &context_stack);
+    // println!("CURRENT FUNC {:?}", current_func);
+    println!("PENDING STATEMENTS {:?}", &pending_statements);
     for inner_pair in pair.into_inner() {
         visualize_pair(
             inner_pair,
@@ -150,7 +157,8 @@ fn visualize_pair(
             semantic_cube,
             context_ids,
             current_func,
-            current_expr
+            quadruples,
+            pending_statements
         );
     }
 
@@ -168,11 +176,11 @@ fn main() {
     // Create semantic cube that will tell us what type of data will be returned when performing an operation
     let semantic_cube = [
         [ // Left operand is int (0)
-            ["int", "float", "int", "float", "int", "int", "int", "int"],  // Right operand int (0) for +, -, *, /, >, <, ==, !=
-            ["float", "float", "float", "float", "int", "int", "int", "int"], // Right operand float (1) for +, -, *, /, >, <, ==, !=
+            ["int", "int", "int", "float", "int", "int", "int", "int"],  // Right operand int (0) for +, -, *, /, >, <, ==, !=
+            ["float", "float", "float", "float", "err", "err", "err", "err"], // Right operand float (1) for +, -, *, /, >, <, ==, !=
         ],
         [ // Left operand is float (1)
-            ["float", "float", "float", "float", "int", "int", "int", "int"], // Right operand int (0) for +, -, *, /, >, <, ==, !=
+            ["float", "float", "float", "float", "err", "err", "err", "err"], // Right operand int (0) for +, -, *, /, >, <, ==, !=
             ["float", "float", "float", "float", "int", "int", "int", "int"], // Right operand float (1) for +, -, *, /, >, <, ==, !=
         ],
     ];
@@ -183,16 +191,17 @@ fn main() {
     let mut context_stack: Vec<Rule> = Vec::new();
     let mut context_ids: Vec<String> = Vec::new();
     let mut current_func: String = String::new();
-    let mut current_expr: VecDeque<[&str; 4]> = VecDeque::new();
+    let mut pending_statements: Vec<String> = Vec::new();
     let main_rules: HashSet<Rule> = [Rule::program, Rule::vars, Rule::funcs, Rule::funcs, Rule::parameters, Rule::statement]
         .iter()
         .cloned()
         .collect();
 
+    let mut quadruples: VecDeque<[&str; 4]> = VecDeque::new();
+
     match PatitoParser::parse(Rule::program, &patito_file) {
         Ok(pairs) => {
             for pair in pairs {
-                // println!("{:#?}", pair.into_inner());
                 visualize_pair(
                     pair,
                     &mut context_stack,
@@ -202,7 +211,8 @@ fn main() {
                     &semantic_cube,
                     &mut context_ids,
                     &mut current_func,
-                    &mut current_expr
+                    &mut quadruples,
+                    &mut pending_statements
                 );
             }
             println!("{:#?}", func_dir);
