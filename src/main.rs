6ba5ac81c32aec3ee2aa1d4f135e1b62b39f49fc
SamAcosta02 <A00833547@tec.mx>
2024-11-05 21:42:59 -0600

    continued developing expression semantics

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::{HashMap, HashSet, VecDeque}, fs};
+use std::{collections::{HashMap, HashSet, VecDeque}, fs, result};
 
 use pest::Parser;
 use pest_derive::Parser;
@@ -6,6 +6,53 @@ use pest_derive::Parser;
 #[derive(Parser)]
 #[grammar = "patito.pest"]
 pub struct PatitoParser;
+
+#[derive(Debug)]
+struct QuadStack {
+    o: Vec<[String; 2]>,
+    oper: Vec<String>,
+    jumps: Vec<i32>
+}
+
+impl QuadStack {
+    fn new() -> Self {
+        QuadStack {
+            o: Vec::new(),
+            oper: Vec::new(),
+            jumps: Vec::new()
+        }
+    }
+}
+
+fn consulut_id_type(func_dir: &mut HashMap<String, HashMap<String, String>>, current_func: &String, pair: &pest::iterators::Pair<Rule>) -> String {
+    // Consult the inner value
+    if let Some(inner_map) = func_dir.get(current_func) {
+        if let Some(value) = inner_map.get(pair.as_str()) {
+            println!("ID type: {:?}", value);
+            value.to_string()
+        } else {
+            "ERROR".to_string()
+        }
+    } else {
+        "ERROR".to_string()
+    }
+}
+
+fn get_type_int(type_str: &str) -> usize {
+    match type_str {
+        "int" => 0,
+        "float" => 1,
+        "+" => 0,
+        "-" => 1,
+        "*" => 2,
+        "/" => 3,
+        ">" => 4,
+        "<" => 5,
+        "==" => 6,
+        "!=" => 7,
+        _ => 10
+    }
+}
 
 fn visualize_pair(
     pair: pest::iterators::Pair<Rule>,
@@ -16,8 +63,9 @@ fn visualize_pair(
     semantic_cube: &[[[&str; 8]; 2]; 2],
     context_ids: &mut Vec<String>,
     current_func: &mut String,
-    quadruples: &mut VecDeque<[&str; 4]>,
-    pending_statements: &mut Vec<String>
+    quadruples: &mut VecDeque<[String; 4]>,
+    pending_statements: &mut Vec<String>,
+    quad_stack: &mut QuadStack
 ) {
 
     // visualize current rule
@@ -77,6 +125,11 @@ fn visualize_pair(
                         // Add the parameter ID to the contex_ids stack
                         context_ids.push(pair.as_str().to_string());
                     },
+                    Rule::factor => {
+                        // Add the factor ID to the quad operand stack
+                        println!(" - - expression action #1");
+                        quad_stack.o.push([pair.as_str().to_string(), consulut_id_type(func_dir, current_func, &pair)]);
+                    }
                     _ => {
                         // if the id is not found in any of these contexts, check if the id exists in the current context
                         println!("Rule is: {:?}", parent_rule);
@@ -113,29 +166,28 @@ fn visualize_pair(
             // println!("ACTION: Changing back to the global context \n");
             *current_func = "global".to_string();
         },
-        Rule::assign => {
-            println!("QUAD INFO: assignment going on here");
-            pending_statements.push("assign".to_string());
+        Rule::operator => {
+            println!(" - - expression action #2");
+            quad_stack.oper.push(pair.as_str().to_string());
         }
-        Rule::condition => {
-            println!("QUAD INFO: condition going on here");
-            pending_statements.push("condition".to_string());
+        Rule::sign => {
+            println!(" - - expression action #3");
+            quad_stack.oper.push(pair.as_str().to_string());
         }
-        Rule::while_loop => {
-            println!("QUAD INFO: while_loop going on here");
-            pending_statements.push("while_loop".to_string());
-        }
-        Rule::func_call => {
-            println!("QUAD INFO: func_call going on here");
-            pending_statements.push("func_call".to_string());
-        }
-        Rule::print => {
-            println!("QUAD INFO: print going on here");
-            pending_statements.push("print".to_string());
-        }
-        Rule::delimiter => {
-            pending_statements.pop();
-            println!("QUAD INFO: current statement is now: {:?} \n", pending_statements.last());
+        Rule::term => {
+            println!(" - - expression action #4");
+            // println!("qud stack: {:?}", &quad_stack);
+            // let right_operand = quad_stack.o.pop().unwrap();
+            // println!("- - - - right operand: {:?}", &right_operand);
+            // let left_operand = quad_stack.o.pop().unwrap();
+            // println!("- - - - right operand: {:?}", &left_operand);
+            // let operator = quad_stack.oper.pop().unwrap();
+            // println!("- - - - operator: {:?}", &operator);
+            // let result_type = semantic_cube[get_type_int(&left_operand[1])][get_type_int(&right_operand[1])][get_type_int(&operator)];
+            // println!("- - - - result type: {:?}", &result_type);
+            // if result_type != "err" {
+            //     quadruples.push_back([right_operand[0].clone(), left_operand[0].clone(), operator.clone(), "_".to_string()]);
+            // }
         }
         _ => {
             println!("... \n");
@@ -144,9 +196,11 @@ fn visualize_pair(
     
     // Check current stack status
     // println!("CONTEXT IDs: {:?}", context_ids);
-    // println!("CONTEXT STACK {:?}", &context_stack);
+    println!("CONTEXT STACK {:?}", &context_stack);
     // println!("CURRENT FUNC {:?}", current_func);
-    println!("PENDING STATEMENTS {:?}", &pending_statements);
+    // println!("PENDING STATEMENTS {:?}", &pending_statements);
+    println!("QUAD STACK {:?}", &quad_stack);
+    println!("QUADRUPLES {:?}", &quadruples);
     for inner_pair in pair.into_inner() {
         visualize_pair(
             inner_pair,
@@ -158,10 +212,11 @@ fn visualize_pair(
             context_ids,
             current_func,
             quadruples,
-            pending_statements
+            pending_statements,
+            quad_stack
         );
     }
-
+    
     // Pop the current rule from the context stack, if it is a main rule (once the recursion is done)
     if main_rules.contains(&current_rule) {
         println!("Rule: --{:?}-- Remove context main Rule as we are done", current_rule);
@@ -170,7 +225,8 @@ fn visualize_pair(
 }
 
 fn main() {
-    let path = "C:/Users/wetpe/OneDrive/Documents/_Manual/TEC 8/ducky-language-rust/src/tests/app2.dusty";
+    // File path to read
+    let path = "C:/Users/wetpe/Documents/Tec8/compiladores/ducky-language-rust/src/tests/app2.dusty";
     let patito_file = fs::read_to_string(&path).expect("error reading file");
 
     // Create semantic cube that will tell us what type of data will be returned when performing an operation
@@ -185,22 +241,31 @@ fn main() {
         ],
     ];
 
-    // Create a directory to store the functions and their types
+    // Check what variables are declared in what scope
     let mut func_dir: HashMap<String, HashMap<String, String>> = HashMap::new();
+    // Check the current type of the variable, to assign it once it/they are declared
     let mut current_type = String::new();
+    // Check the current context of the program (main rules)
     let mut context_stack: Vec<Rule> = Vec::new();
+    // Check what and how many ids to assign a certain type to once a type is declared
     let mut context_ids: Vec<String> = Vec::new();
+    // Check what scope we are currently in (check for ids in this scope) 
     let mut current_func: String = String::new();
+    // TBD
     let mut pending_statements: Vec<String> = Vec::new();
-    let main_rules: HashSet<Rule> = [Rule::program, Rule::vars, Rule::funcs, Rule::funcs, Rule::parameters, Rule::statement]
+    // TBD
+    let mut quad_stack: QuadStack = QuadStack::new();
+    // What main rules to consider to trigger specific actions
+    let main_rules: HashSet<Rule> = [Rule::program, Rule::vars, Rule::funcs, Rule::funcs, Rule::parameters, Rule::factor, Rule::expression]
         .iter()
         .cloned()
         .collect();
 
-    let mut quadruples: VecDeque<[&str; 4]> = VecDeque::new();
+    let mut quadruples: VecDeque<[String; 4]> = VecDeque::new();
 
     match PatitoParser::parse(Rule::program, &patito_file) {
         Ok(pairs) => {
+            // Go through the AST and trigger actions in certain parts
             for pair in pairs {
                 visualize_pair(
                     pair,
@@ -212,13 +277,14 @@ fn main() {
                     &mut context_ids,
                     &mut current_func,
                     &mut quadruples,
-                    &mut pending_statements
+                    &mut pending_statements,
+                    &mut quad_stack
                 );
             }
             println!("{:#?}", func_dir);
         }
         Err(e) => {
-            println!("Error: {:?}", e);
+            println!("Error: {:#?}", e);
         }
     }
 }
